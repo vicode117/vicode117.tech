2de59c1a572e7650f47b230a667e38f3f72fccb1
Victor <vicode117@163.com>
2023-02-13 00:38:12 +0800

    feat(mdn-learning): add path structure

--- src/pages/learning/mdn-learning/index.vue
+++ src/pages/learning/mdn-learning/index.vue
@@ -7,9 +7,7 @@ const router = useRouter()
 const { t } = useI18n()
 
 const subjectList = [
-  { name: 'home.interesting_technology', link: '/interesting_technology' },
-  { name: 'home.work_issues', link: '/work_issues' },
-  { name: 'home.learning', link: '/learning' },
+  { name: 'mdn_learning.html_basic', link: '/learning/mdn-learning/html-basic' },
 ]
 
 const go = (name: string) => {
@@ -18,14 +16,19 @@ const go = (name: string) => {
 </script>
 
 <template>
-  <h1>Vicode117's Blog</h1>
+  <h1>MDN learning</h1>
   <hr>
   <h2 v-for="item in subjectList" :key="item.name" @click="go(item.link)">
     {{ t(item.name) }}
   </h2>
+  <div>
+    <button btn text-sm m="3 t8" @click="router.back()">
+      {{ t('button.back') }}
+    </button>
+  </div>
 </template>
 
 <route lang="yaml">
 meta:
-  layout: home
+  layout: default
 </route>
